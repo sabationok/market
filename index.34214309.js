b77bd720d33f2f16f0caed4da468e4f8a411f3ab
sabationok <110983329+sabationok@users.noreply.github.com>
2022-09-10 23:01:27 +0000

    Deploying to gh-pages from @ sabationok/market@8db3b7a350a2121f410a639e999f7731ce43964f 🚀

--- index.34214309.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slick__brands-slider").slick({dots:!0,infinite:!0,arrows:!1,slidesToShow:1,slidesToScroll:1,prevArrow:'<button type="button" class="slick-prev">&#60;</button>',nextArrow:'<button type="button" class="slick-next">&#62;</button>'})}));
-//# sourceMappingURL=index.34214309.js.map
